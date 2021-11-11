55821f81538897a33af69705a4b0689ceb5868a0
Mark <lindexe@gmail.com>
2021-11-11 22:37:36 +0300

    Update settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='g88)o4(lhoe-&98o_2hn5f-+y!bvu
 DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
-    '84.201.181.59',
+    '178.154.240.68',
     'localhost',
     'web',
 ]
